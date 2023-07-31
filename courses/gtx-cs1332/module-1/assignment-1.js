da5c90791f6f3023eb1cc7e12d5b127b60e3cf18
Justin Seawell <jpseawell@gmail.com>
2023-07-31 10:03:34 -0500

    feat: add remove methods to ArrayList class

--- courses/gtx-cs1332/module-1/assignment-1.js
+++ courses/gtx-cs1332/module-1/assignment-1.js
@@ -25,12 +25,22 @@ class ArrayList {
     return this.capacity === this.count;
   }
 
+  get isEmpty() {
+    return this.count === 0;
+  }
+
   shiftRight() {
     for (let i = this.count - 1; i >= 0; i--)
       if (this.backingArray[i]) this.backingArray[i + 1] = this.backingArray[i];
   }
 
+  validate(value) {
+    if (value == null || value === undefined)
+      throw new Error("Value is null or undefined");
+  }
+
   addToFront(val) {
+    this.validate(val);
     if (this.isFull) this.resize();
     this.count++;
     this.shiftRight();
@@ -38,26 +48,53 @@ class ArrayList {
   }
 
   addToBack(val) {
+    this.validate(val);
     if (this.isFull) this.resize();
     this.count++;
     this.backingArray[this.count - 1] = val;
   }
 
-  // TODO: Implement remove front/back
+  shiftLeft() {
+    for (let i = 0; i <= this.count; i++)
+      this.backingArray[i] = this.backingArray[i + 1];
+  }
+
+  removeFromFront() {
+    if (this.isEmpty) return null;
+    this.count--;
+    const first = this.backingArray[0];
+    this.shiftLeft();
+    return first;
+  }
+
+  removeFromBack() {
+    if (this.isEmpty) return null;
+    this.count--;
+    const last = this.backingArray[this.count];
+    this.backingArray[this.count] = undefined;
+    return last;
+  }
 }
 
 const INITIAL_CAPACITY = 9;
 const list = new ArrayList(INITIAL_CAPACITY);
 
-list.addToFront(3);
-list.addToFront(8);
-list.addToFront(5);
+list.addToBack(0);
 list.addToBack(1);
-list.addToBack(14);
-list.addToBack(0);
-list.addToBack(0);
-list.addToBack(0);
-list.addToBack(0);
-console.log("before:", list.backingArray);
-list.addToBack(0);
-console.log("after:", list.backingArray);
+list.addToBack(2);
+list.addToBack(3);
+list.addToBack(4);
+list.addToBack(5);
+list.addToBack(6);
+list.addToBack(7);
+list.addToBack(8);
+
+console.log(list.backingArray);
+
+list.removeFromFront();
+list.removeFromFront();
+list.removeFromFront();
+list.removeFromBack();
+list.removeFromBack();
+
+console.log(list.backingArray);
