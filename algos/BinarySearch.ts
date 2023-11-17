e80d80e65611754a89d3c85de715b8db21cd73c5
Justin Seawell <jpseawell@gmail.com>
2023-11-17 11:58:37 -0600

    fix: add explanatory notes

--- algos/BinarySearch.ts
+++ algos/BinarySearch.ts
@@ -1,17 +1,32 @@
 export default function binarySearch(
-  haystack: number[],
-  needle: number
+  needle: number,
+  haystack: number[]
 ): boolean {
   let lo = 0;
   let hi = haystack.length;
 
   do {
+    // To find the midpoint:
+    // hi - lo = range
+    // range / 2 = midpoint
+    // lo + midpoint = offset midpoint by the lower bound
+    // (otherwise we'd start at zero but we want to start at lo)
+    // floor (offsetMidpoint) in case it's a decimal
     const m = Math.floor(lo + (hi - lo) / 2);
     const v = haystack[m];
 
     if (v === needle) return true;
-    else if (v > needle) hi = m;
-    else lo = m + 1;
+    else if (v < needle) {
+      // Assume needle is in right half
+      // move lo up to the middle (plus 1)
+
+      lo = m + 1; // inclusive
+    } else {
+      // Assume needle is in left half
+      // move hi down to the middle
+
+      hi = m; // exclusive
+    }
   } while (lo < hi);
 
   return false;
--- algos/twoCrystalBalls.ts
+++ algos/twoCrystalBalls.ts
@@ -1,3 +1,6 @@
+// Given two crystal balls that will break if dropped from high enough distance,
+// determine the exact spot in which it will break in the most optimized way
+
 export default function twoCrystalBalls(breaks: boolean[]): number {
   // We hop by square roots in order to beat O(n) time complexity
   const jmpAmount = Math.floor(Math.sqrt(breaks.length));
